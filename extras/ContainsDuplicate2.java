6ed9589e9668803f6a12612f805a920ce08664da
RushikeshGhodke <rushikeshghodke7455@gmail.com>
2025-11-29 11:42:40 +0530

    solved 3 contains duplicate

---
+++ extras/ContainsDuplicate2.java
@@ -0,0 +1,19 @@
+package extras;
+
+import java.util.HashMap;
+
+public class ContainsDuplicate2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> seen = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (seen.containsKey(val) && i - seen.get(val) <= k)
+                return true;
+            seen.put(val, i);
+        }
+
+        return false;
+    }
+
+}
